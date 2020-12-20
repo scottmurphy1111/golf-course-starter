e439331be1a02aab0b4758852123139278830e01
scottmurphy1111 <scottmurphy1111@gmail.com>
2020-12-20 15:02:24 -0500

    Updates

---
+++ precache-manifest.7190d6c0e2efc8238df2cea8d631e3f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332d1606bb059931fbf46730a8d5276e",
+    "url": "/golf-course-starter/index.html"
+  },
+  {
+    "revision": "e00b43bcedb167a76f5d",
+    "url": "/golf-course-starter/static/css/main.2a7ef8e7.chunk.css"
+  },
+  {
+    "revision": "242f7c17bc936fb2baf4",
+    "url": "/golf-course-starter/static/js/2.8f54d5d8.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/golf-course-starter/static/js/2.8f54d5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00b43bcedb167a76f5d",
+    "url": "/golf-course-starter/static/js/main.7621b52f.chunk.js"
+  },
+  {
+    "revision": "bb35ffbbc622829197f1",
+    "url": "/golf-course-starter/static/js/runtime-main.68fb41d2.js"
+  },
+  {
+    "revision": "922476142dd1ecd5089e842c37bf3726",
+    "url": "/golf-course-starter/static/media/become-member-color.92247614.jpg"
+  },
+  {
+    "revision": "5918689550a260763360428816ccbdfb",
+    "url": "/golf-course-starter/static/media/book-now-color.59186895.jpg"
+  },
+  {
+    "revision": "c7b390cb0969c1a7a9747fb304b3d194",
+    "url": "/golf-course-starter/static/media/book-now.c7b390cb.jpg"
+  },
+  {
+    "revision": "bdb91b8cb9b079d3720988a66e7cf89b",
+    "url": "/golf-course-starter/static/media/content-box.bdb91b8c.jpg"
+  },
+  {
+    "revision": "06bf4c02c140347d9d4d79eae0a26dcf",
+    "url": "/golf-course-starter/static/media/course-with-class.06bf4c02.svg"
+  },
+  {
+    "revision": "964414e4211d829f43541dcb51b16d7f",
+    "url": "/golf-course-starter/static/media/golf-course-bg.964414e4.jpg"
+  },
+  {
+    "revision": "6383153f8fe1c8aae5950f0ed7c508cb",
+    "url": "/golf-course-starter/static/media/logo-golfball.6383153f.svg"
+  },
+  {
+    "revision": "02ebbbba46b169437e5e9f16d8c72945",
+    "url": "/golf-course-starter/static/media/see-course-color.02ebbbba.jpg"
+  }
+]);
